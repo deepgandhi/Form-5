0fa6ca3b7236c8ef254f4656b3b71bea6000172a
Deep Gandhi <deep45596@gmail.com>
2019-01-05 21:56:11 +0530

    que 3

--- form1.py
+++ form1.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 import y
 
-class BankDetails(ttk.Frame):
+class BankDetails(ttk.Labelframe):
 	def __init__(self,parent,*args,**kwargs):
-		super().__init__(parent,*args,**kwargs)
+		super().__init__(parent,text="Bank Details",*args,**kwargs)
 		self.variables={}
 		self.variables["formFiveBankId"]=tk.StringVar()
 		self.variables["formFiveBankBranchName"]=tk.StringVar()
@@ -18,15 +18,28 @@ class BankDetails(ttk.Frame):
 		self.variables['formFiveBankDeposit']=tk.IntVar()
 		self.variables['formFiveBankWithdrawals']=tk.IntVar()
 		
-		ttk.Label(self,text='Bank Details').grid(row=1,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Label(self,text="Bank Name*").grid(row=2,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Entry(self,textvariable=self.variables["formFiveBankId"]).grid(row=2,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Label(self,text='Branch Name*').grid(row=2,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Entry(self,textvariable=self.variables["formFiveBankBranchName"]).grid(row=2,column=4,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Label(self,text='Account Name.').grid(row=3,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Entry(self,textvariable=self.variables["formFiveBankAccountName"]).grid(row=3,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Label(self,text='Account No*').grid(row=3,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
-		ttk.Entry(self,textvariable=self.variables["formFiveBankAccountNo"]).grid(row=3,column=4,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text="Bank Name*").grid(row=0,column=0,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables["formFiveBankId"]).grid(row=0,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Branch Name*').grid(row=0,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables["formFiveBankBranchName"]).grid(row=0,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Account Name.*').grid(row=1,column=0,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables["formFiveBankAccountName"]).grid(row=1,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Account No*').grid(row=1,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables["formFiveBankAccountNo"]).grid(row=1,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Account Type.*').grid(row=2,column=0,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables['formFiveBankAccountType']).grid(row=2,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='IFSC Code*').grid(row=2,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables['formFiveBankIFSCCode']).grid(row=2,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Opening Balance*').grid(row=3,column=0,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables['formFiveBankOpeningBal']).grid(row=3,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Deposit during the period*').grid(row=3,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables['formFiveBankWithdrawals']).grid(row=3,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Withdrawals during the period*').grid(row=4,column=0,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self,textvariable=self.variables['formFiveBankWithdrawals']).grid(row=4,column=1,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Label(self,text='Closing Balance*').grid(row=4,column=2,sticky=(tk.N+tk.E+tk.W+tk.S))
+		ttk.Entry(self).grid(row=4,column=3,sticky=(tk.N+tk.E+tk.W+tk.S))
+		
+		
 		
 
 
@@ -39,7 +52,7 @@ class Application(tk.Tk):
 		self.frame=y.VerticalScrolledFrame(self)
 		self.frame.pack(fill='both',expand='true')
 		self.Question=[]
-		for i in range(2):
+		for i in range(3):
 			self.Question.append(ttk.Frame(self.frame.interior))
 			self.Question[i].grid(row=i,column=0,sticky=tk.W)
 			tk.Grid.rowconfigure(self.frame.interior,i,weight=1)
@@ -49,10 +62,16 @@ class Application(tk.Tk):
 		ttk.Label(self.Question[0],text="1.Whether separate RERA Bank Account has been opened as envisaged in Gujarat RERA Bank Account Direction, 2018?").grid(row=1,column=1,sticky=tk.W)
 		ttk.Combobox(self.Question[0],textvariable=self.data["que1"],values=["yes","no"]).grid(row=1,column=2,sticky=tk.W)
 		#que2
-		ttk.Label(self.Question[1],text="2. Details of RERA Bank Account as registered with Gujarat RERA (mention all the bank accounts if there has been a change in Bank Account registered with RERA)").grid(row=1,column=1)
+		ttk.Label(self.Question[1],text="2. Details of RERA Bank Account as registered with Gujarat RERA (mention all the bank accounts if there has been a change in Bank Account registered with RERA)").grid(row=1,column=1,sticky=tk.W)
 		ttk.Button(self.Question[1],text="Add More",command=self.AddBankDetails).grid(row=2,column=1,sticky=tk.W)
 		self.BankDetailsFrame=ttk.Frame(self.Question[1])
 		self.BankDetailsFrame.grid(row=3,column=1,sticky=tk.W)
+		#que3
+		self.data['que3']=tk.StringVar()
+		ttk.Label(self.Question[2],text='3.	In case of change in RERA Bank Account as indicated above, whether due approval following prescribed documentation was taken from Gujarat RERA under the Gujarat RERA Bank Account Directions, 2018?').grid(row=1,column=1,sticky=tk.W)
+		ttk.Combobox(self.Question[2],textvariable=self.data['que3'],values=['yes','no','Not Changed']).grid(row=1,column=2,sticky=tk.W)
+		
+		
 		
 	def AddBankDetails(self):
 		BankDetails(self.BankDetailsFrame).grid(sticky=tk.W)
